590ac61c1fec9c613aa74bab85116d7df4f8836b
Carlos Eduardo Chanona Aquino <62265673+ShrkyChanona@users.noreply.github.com>
2022-07-11 22:38:11 -0500

    Add files via upload

---
+++ Pintegrador/src/pintegrador/Pintegrador.java
@@ -0,0 +1,14 @@
+
+package pintegrador;
+
+import javax.swing.JButton;
+
+
+public class Pintegrador {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.setVisible(true);
+    }
+    
+}
